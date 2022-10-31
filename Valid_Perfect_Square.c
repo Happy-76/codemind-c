b2d330a4086193f5f994572bb26e8e900b01f52a
Mr. HAPPY <94671185+Happy-76@users.noreply.github.com>
2022-10-31 23:11:58 +0530

    #include<stdio.h> int main() {     int i,n,a[100],c,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=1;j<a[i];j++)         {             if(j*j==a[i])             {                 c++;             }         }         if(c==1)         {             printf("True");         }         else         {             printf("False");         }         printf(" ");     } }

---
+++ Valid_Perfect_Square.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a[100],c,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=1;j<a[i];j++)
+        {
+            if(j*j==a[i])
+            {
+                c++;
+            }
+        }
+        if(c==1)
+        {
+            printf("True");
+        }
+        else
+        {
+            printf("False");
+        }
+        printf("
+");
+    }
+}
