08a86055143f843dd96db45d15c6843364a9953d
Mr. HAPPY <94671185+Happy-76@users.noreply.github.com>
2022-11-01 20:49:18 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     string s;     cin>>s;     if(s[0]!='0' && s.size()==10)     cout<<"Valid"<<endl;     else     cout<<"Invalid"<<endl;     return 0; }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    if(s[0]!='0' && s.size()==10)
+    cout<<"Valid"<<endl;
+    else
+    cout<<"Invalid"<<endl;
+    return 0;
+}
