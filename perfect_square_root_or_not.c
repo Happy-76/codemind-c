994b723f8a9b653cd9414af91866d1da53cc2160
Mr. HAPPY <94671185+Happy-76@users.noreply.github.com>
2022-11-01 20:56:52 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int num;     cin>>num;     int sq=sqrt(num);     if(sq*sq==num)     cout<<"True"<<endl;     else     cout<<"False"<<endl;     return 0; }

---
+++ perfect_square_root_or_not.c
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    int sq=sqrt(num);
+    if(sq*sq==num)
+    cout<<"True"<<endl;
+    else
+    cout<<"False"<<endl;
+    return 0;
+}
