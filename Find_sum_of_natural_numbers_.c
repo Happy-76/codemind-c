2fcd682dc3d23f88a1998bfc37baa591f7d21868
Mr. HAPPY <94671185+Happy-76@users.noreply.github.com>
2022-11-02 23:37:08 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int num;     int sum=0;     cin>>num;     for(int i=0;i<=num;i++){         sum+=i;     }     cout<<sum;     return 0; }

---
+++ Find_sum_of_natural_numbers_.c
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    int sum=0;
+    cin>>num;
+    for(int i=0;i<=num;i++){
+        sum+=i;
+    }
+    cout<<sum;
+    return 0;
+}
