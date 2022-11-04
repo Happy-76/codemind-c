d2306bd417296d1575755c62188a23efb869ac7a
Mr. HAPPY <94671185+Happy-76@users.noreply.github.com>
2022-11-04 19:45:13 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int m,n;     cin>>m>>n;     if(m%2==0 || n%2==0)     {         cout<<"Player 1";     }     else     {         cout<<"Player 2";     } }

---
+++ Predict_the_winner_in_Coin_Game.c
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+    if(m%2==0 || n%2==0)
+    {
+        cout<<"Player 1";
+    }
+    else
+    {
+        cout<<"Player 2";
+    }
+}
